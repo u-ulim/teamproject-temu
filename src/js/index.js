4bf498d6597ae3785b9050fffd6deeafc6576c58
쩡뽀꼉 <bbok@jjeongppokkyeongs-MacBook-Pro.local>
2024-08-11 17:05:31 +0900

    bk_product

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+// product-list.html 파일을 로드하여 #product 안에 삽입
+const productListLoad = () => {
+    const productList = document.getElementById('product-list');
+    fetch('../../html/components/product-list.html')
+        .then(response => response.text())
+        .then(data => {
+            productList.innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+}
+
+productListLoad();
