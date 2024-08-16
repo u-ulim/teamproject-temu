d01d1e4766394bcb3dabc0bd4ad7c85ee412dd1f
u-ulim <uiissoop@gmail.com>
2024-08-16 19:44:02 +0900

    dk_review-design

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,10 @@
 // product-list.html 파일을 로드하여 #product 안에 삽입
 const headerLoad = () => {
-  const productList = document.getElementById("header");
+  const header = document.querySelector("#header");
   fetch("../../html/components/header.html")
     .then((response) => response.text())
     .then((data) => {
-      productList.innerHTML = data;
+      header.innerHTML = data;
     })
     .catch((error) => console.error("Error loading header:", error));
 };
--- src/json/category-list.js
+++ src/json/category-list.js
@@ -1,8 +1,8 @@
 // JSON 데이터 변수
-console.log("hi");
 const categoryURL =
   "https://my-json-server.typicode.com/u-ulim/temu-category/db";
 
+console.log("Hi");
 // JSON 데이터 패치하기
 fetch(categoryURL)
   .then((response) => response.json())
@@ -17,31 +17,35 @@ fetch(categoryURL)
       });
     };
     importData();
+    // 마우스 오버 시 카테고리 리스트 출력
+    const tabs = document.querySelector(".submenu-tabs");
+    console.log(tabs);
+
+    document.querySelectorAll(".submenu-tabs li").forEach((tab) => {
+      tab.addEventListener("mouseover", function () {
+        console.log("hi");
+
+        // 모든 탭에서 활성화 클래스 제거
+        document
+          .querySelectorAll(".submenu-tabs li")
+          .forEach((t) => t.classList.remove("active"));
+
+        // 현재 탭에 활성화 클래스 추가
+        this.classList.add("active");
+
+        // 모든 콘텐츠에서 활성화 클래스 제거
+        document
+          .querySelectorAll(".submenu-content .content")
+          .forEach((content) => {
+            content.classList.remove("active");
+          });
+
+        // 현재 탭과 연결된 콘텐츠를 활성화
+        const tabContentId = this.getAttribute("data-tab");
+        document.getElementById(tabContentId).classList.add("active");
+      });
+    });
   })
   .catch((error) => {
     console.log(error);
   });
-
-// 마우스 오버 시 카테고리 리스트 출력
-document.querySelectorAll(".submenu-tabs li").forEach((tab) => {
-  tab.addEventListener("mouseover", function () {
-    // 모든 탭에서 활성화 클래스 제거
-    document
-      .querySelectorAll(".submenu-tabs li")
-      .forEach((t) => t.classList.remove("active"));
-
-    // 현재 탭에 활성화 클래스 추가
-    this.classList.add("active");
-
-    // 모든 콘텐츠에서 활성화 클래스 제거
-    document
-      .querySelectorAll(".submenu-content .content")
-      .forEach((content) => {
-        content.classList.remove("active");
-      });
-
-    // 현재 탭과 연결된 콘텐츠를 활성화
-    const tabContentId = this.getAttribute("data-tab");
-    document.getElementById(tabContentId).classList.add("active");
-  });
-});
