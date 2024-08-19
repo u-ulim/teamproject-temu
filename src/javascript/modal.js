0f1a45059cbb68473c58e17badfd84095d1cee74
jisumoon <jjisu97@naver.com>
2024-08-19 18:21:08 +0900

    js-13th

--- src/javascript/modal.js
+++ src/javascript/modal.js
@@ -7,20 +7,19 @@ bannerClose.addEventListener("click", () => {
 });
 
 // 옵션 변경 시 모달 창
-if (document.querySelector(".option") === true) {
-  const optionSeclect = document.querySelector(".option");
-  const optionModal = document.querySelector(".modal__box");
-  const modalClose = document.querySelector(".modal__close-box");
 
-  if (optionSeclect && optionModal && modalClose) {
-    optionSeclect.addEventListener("click", () => {
-      optionModal.classList.add("active");
-    });
+const optionSeclect = document.querySelector(".option");
+const optionModal = document.querySelector(".modal__box");
+const modalClose = document.querySelector(".modal__close-box");
 
-    modalClose.addEventListener("click", () => {
-      optionModal.classList.remove("active");
-    });
-  }
+if (optionSeclect && optionModal && modalClose) {
+  optionSeclect.addEventListener("click", () => {
+    optionModal.classList.add("active");
+  });
+
+  modalClose.addEventListener("click", () => {
+    optionModal.classList.remove("active");
+  });
 }
 
 // 주소 변경 모달 창
@@ -39,3 +38,21 @@ if (addressBtn && addressModal && addressClose) {
 }
 
 // 주소 추가 모달 창
+const addressPlusBtn = document.querySelector("#plus-address");
+const addressPlusModal = document.querySelector(".address__modal__box-plus");
+const addressPlusClose = document.querySelector(".address__close-box-plus");
+const addressBackBtn = document.querySelector(".address__back-box");
+
+addressPlusBtn.addEventListener("click", () => {
+  addressPlusModal.classList.add("active");
+});
+
+addressPlusClose.addEventListener("click", () => {
+  addressPlusModal.classList.remove("active");
+  addressModal.classList.remove("active");
+});
+
+addressBackBtn.addEventListener("click", () => {
+  addressPlusModal.classList.remove("active");
+  addressModal.classList.add("active");
+});
