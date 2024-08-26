f94259aa81d34b645d02bddc96497d8c7916059e
jbbok <qkrwjd94@gmail.com>
2024-08-26 21:45:36 +0900

    bk_main-slide

--- src/json/script.js
+++ src/json/script.js
@@ -1,51 +1,121 @@
-const productsURL =
-  "https://my-json-server.typicode.com/u-ulim/temu-products/db";
+const btns = document.querySelector(".main__slide-btn");
+const prevBtn = document.querySelector(".main__slide-btn-right");
+const nextBtn = document.querySelector(".main__slide-btn-left");
+console.log(btns, prevBtn, nextBtn);
 
-console.log("hi");
+const slides = document.querySelector(".main__slides");
+const slide = slides.querySelectorAll("li");
 
-fetch(productsURL)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
+console.log(slides, slide);
 
-    const createItem = (product) => {
-      const ul = document.querySelector(".product-lists");
-      const productList = document.createElement("li");
-      const productListTitle = document.createElement("h3");
-      const productListThumb = document.createElement("img");
-      const productListPrice = document.createElement("span");
-      const div = document.createElement("div");
-      const attr = document.createAttribute("src");
-      const price = new Intl.NumberFormat("ko-kr", {}).format(product.price);
+const slideCount = slide.length;
+const slideWidth = 447;
+const slideMargin = 30;
 
-      productList.id = product.id;
+const mainSlidePg = document.querySelector(".number");
 
-      productListTitle.className = "prodcut-list__title";
-      productListTitle.innerText = product.title;
+console.log(slideCount, slideWidth, slideMargin);
 
-      productListPrice.className = "price";
-      productListPrice.innerText = price;
+let currentIdx = 0;
 
-      attr.value = product.thumbnail;
-      productListThumb.setAttributeNode(attr);
-      ul.appendChild(productList);
-      div.append(productListTitle, productListPrice);
-      productList.append(productListThumb, div);
+// 복제한 5개의 li노드를 왼쪽으로 이동시키기 위한 함수(1)
+const updateWidth = () => {
+  const currentSlides = document.querySelectorAll(".slides li");
+  const newSlideCount = currentSlides.length;
+  const newWidth = `${
+    (slideWidth + slideMargin) * newSlideCount - slideMargin
+  }px`;
+  slides.style.width = newWidth;
+};
 
-      productList.addEventListener("click", () => {
-        const url = `/html/product.html?category=${
-          product.category
-        }&name=${encodeURIComponent(product.name)}`;
-        window.location.href = url;
-      });
-    };
-    const importData = () => {
-      data.products.map((product) => {
-        createItem(product);
-      });
-    };
-    importData();
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// 복제한 5개의 li노드를 왼쪽으로 이동시키기 위한 함수(2)
+const setInitialPos = () => {
+  const initialTranslateValue = -(slideWidth + slideMargin) * slideCount;
+  slides.style.transform = `translateX(${initialTranslateValue}px)`;
+};
+
+// li노드를 복제하기 위한 함수
+const makeClone = () => {
+  for (let i = 0; i < slideCount; i++) {
+    const cloneSlide = slide[i].cloneNode(true);
+    cloneSlide.classList.add("clone");
+    slides.appendChild(cloneSlide);
+  }
+  for (let i = slideCount - 1; i >= 0; i--) {
+    const cloneSlide = slide[i].cloneNode(true);
+    cloneSlide.classList.add("clone");
+    slides.prepend(cloneSlide);
+  }
+
+  updateWidth();
+  setInitialPos();
+  setTimeout(() => {
+    slides.classList.add("animated");
+  }, 100);
+};
+
+makeClone();
+
+// 버튼 클릭을 통해서 실제 슬라이드를 출력시켜주는 함수
+const moveSlide = (num) => {
+  console.log(num);
+
+  slides.style.left = `${-num * (slideWidth + slideMargin)}px`;
+  currentIdx = num;
+  if (currentIdx === slideCount || currentIdx === -slideCount) {
+    setTimeout(() => {
+      slides.classList.remove("animated");
+      slides.style.left = "0px";
+      currentIdx = 0;
+    }, 500);
+    setTimeout(() => {
+      slides.classList.add("animated");
+    }, 600);
+  }
+};
+
+// 버튼 클릭 이벤트 함수
+nextBtn.addEventListener("click", () => {
+  moveSlide(currentIdx + 1);
+});
+
+prevBtn.addEventListener("click", () => {
+  console.log("click");
+  moveSlide(currentIdx - 1);
+  mainSlidePg.innerText = `${currentIdx + 1} / 4`;
+});
+
+// 자동슬라이드 및 정지기능 함수
+
+let timer = undefined;
+
+const autoSlide = () => {
+  if (timer === undefined) {
+    timer = setInterval(() => {
+      moveSlide(currentIdx + 1);
+    }, 3000);
+  }
+};
+
+autoSlide();
+
+const stopSlide = () => {
+  clearInterval(timer);
+  timer = undefined;
+};
+
+slides.addEventListener("mouseenter", () => {
+  stopSlide();
+});
+
+slides.addEventListener("mouseleave", () => {
+  autoSlide();
+});
+
+btns.addEventListener("mouseenter", () => {
+  stopSlide();
+});
+
+btns.addEventListener("mouseleave", () => {
+  autoSlide();
+});
