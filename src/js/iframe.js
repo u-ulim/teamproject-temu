c34398abe0f755109a19b1df02a3aee0a52ee259
김정하 <gimjeongha@gimjeonghaui-MacBookPro.local>
2024-08-30 19:56:12 +0900

    signup_page

--- src/js/iframe.js
+++ src/js/iframe.js
@@ -1,3 +1,13 @@
-function switchIframe(page) {
-  document.getElementById("contentFrame").src = page;
-}
+const reviewPage = document.querySelector("#menuRiview");
+
+const reviewCompletePage = document.querySelector("#menuRiveiwComplete");
+
+console.log("hi");
+
+reviewPage.addEventListener("click", () => {
+  console.log("click");
+});
+
+reviewCompletePage.addEventListener("click", () => {
+  console.log("click");
+});
