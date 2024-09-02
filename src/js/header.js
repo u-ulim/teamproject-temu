278af3553e471a2cc2d82649e4b6c205d428922c
u-ulim <uiissoop@gmail.com>
2024-09-02 12:43:19 +0900

    dk_update

--- src/js/header.js
+++ src/js/header.js
@@ -81,6 +81,75 @@ const headerLoad = () => {
         });
       };
       headerRollingEvt();
+
+      // header mobile overflow menu
+      const hashContent = document.querySelector(".mobile-menu");
+      const listClientWidth = hashContent.clientWidth;
+      const listScollWidth = hashContent.clientWidth + 200;
+
+      // 최초 터치 및 마우스다운 지점
+      let startX = 0;
+
+      // 현재 이동중인 지점
+      let nowX = 0;
+
+      // 터치 종료 지점
+      let endX = 0;
+
+      // 두번째 터치 지점
+      let listX = 0;
+
+      const getClientX = (e) => {
+        return e.touches ? e.touches[0].clientX : e.clientX;
+      };
+
+      const getTranslateX = () => {
+        return parseInt(
+          getComputedStyle(hashContent).transform.split(/[^\-0-9]+/g)[5]
+        );
+      };
+
+      const setTranslateX = (x) => {
+        hashContent.style.transform = `translateX(${x}px)`;
+      };
+
+      const onScrollMove = (e) => {
+        nowX = getClientX(e);
+        setTranslateX(listX + nowX - startX);
+      };
+
+      const onScrollEnd = (e) => {
+        endX = getClientX(e);
+        listX = getTranslateX();
+        if (listX > 0) {
+          setTranslateX(0);
+          hashContent.style.transition = `all 0.1s ease`;
+          listX = 0;
+        } else if (listX < listClientWidth - listScollWidth) {
+          setTranslateX(listClientWidth - listScollWidth);
+          hashContent.style.transition = `all 0.1s ease`;
+          listX = listClientWidth - listScollWidth;
+        }
+
+        window.removeEventListener("touchstart", onScrollStart);
+        window.removeEventListener("mousedown", onScrollStart);
+        window.removeEventListener("touchmove", onScrollMove);
+        window.removeEventListener("mousemove", onScrollMove);
+        window.removeEventListener("touchend", onScrollEnd);
+        window.removeEventListener("mouseup", onScrollEnd);
+      };
+
+      const onScrollStart = (e) => {
+        startX = getClientX(e);
+
+        window.addEventListener("touchmove", onScrollMove);
+        window.addEventListener("mousemove", onScrollMove);
+        window.addEventListener("touchend", onScrollEnd);
+        window.addEventListener("mouseup", onScrollEnd);
+      };
+
+      hashContent.addEventListener("touchstart", onScrollStart);
+      hashContent.addEventListener("mousedown", onScrollStart);
     })
     .catch((error) => console.error("Error loading header:", error));
 };
