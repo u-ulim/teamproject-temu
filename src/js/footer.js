44b35649661aec6f33c10cf1f71ab306e0c04fa2
moonjisu <jjisu97@naver.com>
2024-09-05 20:46:18 +0900

    dk-jh

--- src/js/footer.js
+++ src/js/footer.js
@@ -10,3 +10,30 @@ const footerLoad = () => {
 };
 
 footerLoad();
+
+// 모바일 버전에서 각각의 버튼을 눌렀을 때, 각각의 페이지로 넘어감
+
+const homeBtn = document.querySelector(".footer-m__menu-home");
+const categoryBtn = document.querySelector(".footer-m__menu-category");
+const myBtn = document.querySelector(".footer-m__menu-my");
+const cartBtn = document.querySelector(".footer-m__menu-cart");
+
+homeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "index.html";
+});
+
+categoryBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "index.html";
+});
+
+myBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "index.html";
+});
+
+cartBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "index.html";
+});
